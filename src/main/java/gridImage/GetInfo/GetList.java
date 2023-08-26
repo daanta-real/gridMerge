c22c0cd89e91d0781358ad6837a53d963945c512
Junsung Park / Daanta <daanta@naver.com>
2023-08-26 16:44:54 +0900

    230826_1644 Folder correction

---
+++ src/main/java/gridImage/GetInfo/GetList.java
@@ -0,0 +1,87 @@
+package gridImage.GetInfo;
+
+import gridImage.Pref;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+// Only be used for get file list while gridMerge.GetInfo window is open
+@Slf4j public final class GetList {
+
+    // Instance not allowed
+    private GetList() {}
+
+    // Variables
+    private static JFileChooser fileChooser;
+
+    // Methods
+
+    // Initializer
+    public static void init() {
+
+        fileChooser = new JFileChooser(Pref.PATH) {
+            @Override public void approveSelection() {
+                if (getSelectedFiles().length == 0) {
+                    JOptionPane.showMessageDialog(this,
+                            "No selected file",
+                            "ERROR!",
+                            JOptionPane.ERROR_MESSAGE);
+                    return;
+                }
+                super.approveSelection();
+            }
+        };
+
+        fileChooser.setMultiSelectionEnabled(true);
+        fileChooser.setDialogTitle("File selection");
+
+        // Accepts BMP, JPG, and PNG files
+        FileNameExtensionFilter imageFilter = new FileNameExtensionFilter("Image Files (.bmp, .jpg, .png)", "bmp", "jpg", "png");
+        fileChooser.setFileFilter(imageFilter);
+
+    }
+
+    // File selector
+    @NonNull public static List<String> chooseFiles() {
+
+        init();
+        GetInfo getInfo = gridImage.GetInfo.GetInfo.getInstance();
+
+        List<String> result = new ArrayList<>();
+
+        int returnValue = fileChooser.showOpenDialog(getInfo);
+        log.debug("return value: {}", returnValue);
+
+        switch (returnValue) {
+            case JFileChooser.CANCEL_OPTION -> { }
+            case JFileChooser.ERROR_OPTION -> JOptionPane.showMessageDialog(getInfo, "No file selected.", "Alert", JOptionPane.WARNING_MESSAGE);
+            case JFileChooser.APPROVE_OPTION -> {
+
+                File[] selectedFiles = fileChooser.getSelectedFiles();
+                if (selectedFiles.length == 0) {
+                    JOptionPane.showMessageDialog(
+                            getInfo,
+                            "No selected options. Please choose at least 1 or more.",
+                            "Warning",
+                            JOptionPane.WARNING_MESSAGE
+                    );
+                } else {
+                    for (File f : selectedFiles) {
+                        result.add(f.getAbsolutePath());
+                    }
+                }
+                log.debug("Chosen files: {}", result);
+
+            }
+        }
+
+        return result;
+
+    }
+
+}
---
+++ src/main/java/gridImage/Util.java
@@ -0,0 +1,37 @@
+package gridImage;
+
+import com.google.gson.GsonBuilder;
+import lombok.NonNull;
+import org.junit.platform.commons.util.StringUtils;
+
+import java.util.List;
+
+public final class Util {
+
+    public static boolean isPositiveNumber(String value) {
+
+        try {
+
+            if (StringUtils.isBlank(value)) {
+                return false;
+            }
+
+            int intValue = Integer.parseInt(value);
+            if (intValue <= 0) {
+                return false;
+            }
+
+        } catch(NumberFormatException e) {
+            return false;
+        }
+
+        return true;
+
+    }
+
+    @NonNull
+    public static String getPrettyStringInJson(List<String> list) {
+        return String.format(new GsonBuilder().setPrettyPrinting().create().toJson(list));
+    }
+
+}
